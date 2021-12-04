f11de93a91f2b7dd85c0e58be2ecb8affad4701b
Naman-Saxena1 <namansaxena.official@gmail.com>
2021-12-04 22:47:24 +0530

    Finished Fetch React Practice

--- src/components/fetchComp.js
+++ src/components/fetchComp.js
@@ -6,6 +6,15 @@ function FetchComp()
 
     const [data,setData] = useState([])
     const [specificDataId,setSpecificDataId] = useState([])
+    const [postTitle,setPostTitle] = useState()
+    const [postBody,setPostBody] = useState()
+    const [postUserId,setPostUserId] = useState()
+    const [patchId,setPatchId] = useState()
+    const [patchTitle,setPatchTitle] = useState()
+    const [patchBody,setPatchBody] = useState()
+    const [patchUserId,setPatchUserId] = useState()
+    const [deletePostId,setDeletePostId] = useState()
+    const [filterPostId,setFilterPostId] = useState()
 
     //Get Request for All Data
     function getAllData()
@@ -17,7 +26,6 @@ function FetchComp()
             setData(resData)
         })
     }
-
     
     //Get Request for Specific Data
     function onSpecificDataIdChange(event)
@@ -35,8 +43,131 @@ function FetchComp()
             setData([resData])
         })
     }
-
     
+    //Post Request to Create Data
+    function onPostTitleChange(event)
+    {
+        setPostTitle(event.target.value)
+    }
+    function onPostBodyChange(event)
+    {
+        setPostBody(event.target.value)
+    }
+    function onPostUserIdChange(event)
+    {
+        setPostUserId(event.target.value)
+    }
+    function postData()
+    {
+        fetch(
+                serverAPIUrl,
+                {
+                    method : 'POST',
+                    body : JSON.stringify({
+                        title: `${postTitle}`,
+                        body : `${postBody}`,
+                        userId: Number(postUserId)
+                    }),
+                    headers :{
+                        'Content-type': 'application/json; charset=UTF-8'} 
+                } 
+            )
+        .then(res=>res.json())
+        .then(resData=>{
+            setData([resData])
+        })
+    }
+
+    //Patch Request to Update Existing Specific Data
+    function onPatchIdChange(event)
+    {
+        setPatchId(event.target.value)
+    }
+    function onPatchTitleChange(event)
+    {
+        setPatchTitle(event.target.value)
+    }
+    function onPatchBodyChange(event)
+    {
+        setPatchBody(event.target.value)
+    }
+    function onPatchUserIdChange(event)
+    {
+        setPatchUserId(event.target.value)
+    }
+    function patchData()
+    {
+        let urlOfPostToBePatched = `${serverAPIUrl}/${patchId}`
+        
+        let obj = {
+            id : `${patchId}`,
+            title : `${patchTitle}`,
+            body : `${patchBody}`,
+            userId : `${patchUserId}`
+        }
+
+        let removeEmptyAttributeFunction = (obj) => {
+            for (var propName in obj) {
+            if (obj[propName] === "" || obj[propName] === 'undefined') {
+                delete obj[propName];
+            }
+            }
+            return obj
+        }
+        
+        let patchedObject = removeEmptyAttributeFunction(obj)
+        console.log(patchedObject)
+
+        fetch(
+            urlOfPostToBePatched,
+            {
+                method : 'PATCH',
+                body : JSON.stringify(patchedObject),
+                headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                },
+            }
+        )
+        .then(res=>res.json())
+        .then(resData=>{
+            setData([resData])
+        })
+    }
+
+    //Delete Specific Post
+    function onDeletePostIdChange(event)
+    {
+        setDeletePostId(event.target.value)
+    }
+    function deleteData()
+    {
+        let deletedPostURL = `${serverAPIUrl}/${deletePostId}`
+
+        fetch(deletedPostURL,{method:'DELETE'})
+        .then(res=>{
+            if(res.status==200||res.status==201)
+            {
+                setData([{id:deletePostId,title:"Post deleted",body:"",userId:""}])
+            }
+        })
+    }
+
+    //Filter specific posts for a specific User
+    function onFilterPostIdChange(event)
+    {
+        setFilterPostId(event.target.value)
+    }
+    function filterData()
+    {
+        const filterPostURL = `${serverAPIUrl}?userId=${filterPostId}`
+        
+        fetch(filterPostURL)
+        .then(res=>res.json())
+        .then(resData=>{
+            setData(resData) 
+        })
+    }
+
     return (
         <div>
             <h2>Get All Data</h2>
@@ -46,6 +177,27 @@ function FetchComp()
             <input type="number" value={specificDataId} onChange={onSpecificDataIdChange} id="specificPost" placeholder="Enter number"></input>
             <button id="getSpecificData" onClick={getSpecificData}>Get specific data</button>
             <hr/>
+            <h2>Post Data</h2>
+            <input type="text" value={postTitle} onChange={onPostTitleChange} id="postTitle" placeholder="Enter Title"></input>
+            <input type="text" value={postBody} onChange={onPostBodyChange} id="postBody" placeholder="Enter Body"></input>
+            <input type="number" value={postUserId} onChange={onPostUserIdChange} id="postUserId" placeholder="Enter userId number"></input>
+            <button id="postDataBtn" onClick={postData}>Post data</button>
+            <hr/>
+            <h2>Patch Request (Updating Specific Data)</h2>
+            <input type="number" value={patchId} onChange={onPatchIdChange} id="patchId" placeholder="Enter post id to update"></input>
+            <input type="text" value={patchTitle} onChange={onPatchTitleChange} id="patchTitle" placeholder="Enter Title"></input>
+            <input type="text" value={patchBody} onChange={onPatchBodyChange} id="patchBody" placeholder="Enter Body"></input>
+            <input type="number" value={patchUserId} onChange={onPatchUserIdChange} id="patchUserId" placeholder="Enter userId number"></input>
+            <button id="patchDataBtn" onClick={patchData}>Patch data</button>
+            <hr/>
+            <h2>Delete Specific Data</h2>
+            <input type="number" value={deletePostId} onChange={onDeletePostIdChange} id="deletePost" placeholder="Enter number"></input>
+            <button id="deleteSpecificData" onClick={deleteData}>Delete specific data</button>
+            <hr/>
+            <h2>Filter Specific Data</h2>
+            <input type="number" value={filterPostId} onChange={onFilterPostIdChange} id="filterPostUserId" placeholder="Enter number"></input>
+            <button id="filterSpecificDataBtn" onClick={filterData}>Filter specific data</button>
+            <hr/>
             {console.log(data)}
             <ol id="listOfItems">
                 {
@@ -53,7 +205,9 @@ function FetchComp()
                     <li key={item.id}>
                         <h2>{item.title}</h2>
                         <p>{item.body}</p>
-                        <p> User Id : {item.userId}</p>
+                        {
+                            item.userId?(<p>UserId : {item.userId}</p>):(<></>)
+                        }
                     </li>
                     ))
                 }
